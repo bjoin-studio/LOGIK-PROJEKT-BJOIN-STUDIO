2dfbcfee9e1020da9b557e8e27b6b6bc0c9ee47b
Nick Bjoin <nbjoin@supercomputer.lan>
2026-01-10 15:07:35 -0800

    Update Python path to Flame 2026.2.1

--- run_logikprojekt_linux.sh
+++ run_logikprojekt_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/nbjoin/Documents/workspace/GitHub/bjoin-studio/LOGIK-PROJEKT-BJOIN-STUDIO
 export PYTHONPATH=/home/nbjoin/Documents/workspace/GitHub/bjoin-studio/LOGIK-PROJEKT-BJOIN-STUDIO:
-/opt/Autodesk/python/2026.2/bin/python /home/nbjoin/Documents/workspace/GitHub/bjoin-studio/LOGIK-PROJEKT-BJOIN-STUDIO/src/app.py
+/opt/Autodesk/python/2026.2.1/bin/python /home/nbjoin/Documents/workspace/GitHub/bjoin-studio/LOGIK-PROJEKT-BJOIN-STUDIO/src/app.py
